86fa52ed38bf74e2a844931143a77e08d9749727
Juraj Uhlar <juro.uhlar@gmail.com>
2025-11-09 15:47:47 +0000

    feat: black canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-
-import './App.css'
+// import "./App.css";
+import "@xyflow/react/dist/style.css";
+import { ReactFlow, Background, Controls } from "@xyflow/react";
 
 function App() {
-
   return (
     <>
-      <h1>React Flow Sandbox</h1>
+      {/* <h1>React Flow Sandbox</h1> */}
+      <div style={{ height: "100vh", width: "100vw" }}>
+        <ReactFlow>
+          <Background />
+          <Controls />
+        </ReactFlow>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
